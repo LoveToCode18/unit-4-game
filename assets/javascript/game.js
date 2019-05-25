eb85f4a1f459001309faf2468984957dbc924276
Ronnie Larson <ronnielarson17@gmail.com>
2019-05-25 15:53:36 -0600

    added variables to hold numberoptions and a counter variable equal to zero.  for the id number to guess i made it equal to the target number. I gave the img variable a class of crystal image. I used number options to give the crystals a value.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,28 @@
 var targetNumber = 50;
 
-$('.crystal-image').on("click", function() {
+
+$("#number-to-guess").text(targetNumber);
+
+var counter = 0;
+
+var numberOptions = [12,4,7,3];
+
+for (var i = 0; i < numberOptions.length; i++){
+    var imageCrystal = $("<img>");
+
+    imageCrystal.addClass("crystal-image");
+
+    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+    $("#crystals").append(imageCrystal);
+}
+
+$(".")
     
-    counter += 1;
+   
 
 
-});
+
+
